14cfc85ec2411c84ba460eb765400bbd2ca33cad
Chinseok Lee <me@askcompany.kr>
2019-06-04 11:22:45 +0900

    travel, post_list/post_detail 뷰 구현

--- travel/urls.py
+++ travel/urls.py
@@ -4,4 +4,8 @@ from . import views
 app_name = 'travel'
 
 urlpatterns = [
+    # travel/ => Post List
+    path('', views.post_list, name='post_list'),
+    # travel/숫자/ => Post Detail
+    path('<int:pk>/', views.post_detail, name='post_detail'),
 ]
