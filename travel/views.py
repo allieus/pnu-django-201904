0c3b2e47c1ae86d84eeffa765b2e1e9bdfd867bd
Chinseok Lee <me@askcompany.kr>
2019-06-04 13:44:01 +0900

    travel, comment_new 뷰 개선

--- travel/views.py
+++ travel/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
 from .models import Post, Comment
 from .forms import CommentForm
@@ -16,12 +17,19 @@ def post_detail(request, pk):
     })
 
 
+@login_required
 def comment_new(request, post_pk):
+    post = get_object_or_404(Post, pk=post_pk)
+
     if request.method == 'POST':
         form = CommentForm(request.POST)
         if form.is_valid():
-            comment = form.save()
+            comment = form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
             return redirect('travel:post_detail', post_pk)
     else:
         form = CommentForm()
+
     return render(request, 'travel/comment_form.html', {'form': form})
